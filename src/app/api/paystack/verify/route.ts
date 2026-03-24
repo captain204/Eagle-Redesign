e04be10663ea263731511fcf43608656186b6d59
Nuru <nurudeenakindele8@gmail.com>
2026-03-24 15:50:16 +0100

    Failing Build 2

--- src/app/api/paystack/verify/route.ts
+++ src/app/api/paystack/verify/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyPaystackTransaction } from '@/lib/paystack';
 import { getPayload } from 'payload';
